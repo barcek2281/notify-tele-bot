fad2efa6768de37ea96938977795cad52f1218da
barcek2281 <sabdpp17@gmail.com>
2025-04-24 20:58:40 +0500

    config file works

--- config_data/config.py
+++ config_data/config.py
@@ -1,6 +1,16 @@
 from dataclasses import dataclass
 
+from environs import Env
+
 @dataclass
 class Config:
-    BotToke: str
+    BotToken: str
 
+def load_env(path: str = ".env") -> Config:
+
+    env: Env = Env()
+    env.read_env(path=path)
+
+    return Config(
+        BotToken=env("BOT_TOKEN")
+    )
---
+++ main.py
@@ -0,0 +1,7 @@
+
+
+from config_data import config
+
+conf = config.load_env(".env")
+
+print(conf)
